7b61e2931b8f0710729db4f8068ca3daf32944a7
Lautaro Vergara Amodeo <lautarovergaramodeo@gmail.com>
2024-05-27 16:14:48 -0300

    first commit

---
+++ funcion.py
@@ -0,0 +1,260 @@
+
+# Importaciones
+from pathlib import Path
+from fastapi import Query
+import pandas as pd
+import pyarrow.parquet as pq
+
+
+# Importar los DataFrames desde archivos Parquet
+df_primigenio = pd.read_parquet('df_primigenio.parquet')
+df_ugenre = pd.read_parquet('df_ugenre.parquet')
+df_bdev = pd.read_parquet('df_dvev.parquet')
+df_sentimiento = pq.read_table('df_sentimiento').to_pandas()
+
+
+
+def inicio():
+    '''
+    Página de inicio que muestra una presentación.
+
+    Returns:
+    str: Código HTML que muestra la página de inicio.
+    '''
+    return '''
+    <html>
+        <head>
+            <title>EJECUCION LOCAL API STEAM </title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                p {
+                    color: #666;
+                    text-align: center;
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Bienvenido a la API Steam</h1>
+            <p>Esta es la página de inicio de la API de Steam para consultas de videojuegos.</p>
+        </body>
+    </html>
+    '''
+
+
+
+def UserForGenre(genero: str = Query(..., 
+                                      description="Género de videojuegos", 
+                                      example='Adventure')):
+    """
+    Encuentra al usuario con más horas jugadas para un género específico.
+
+    Parameters:
+    - genero (str): Género de videojuegos.
+
+    Returns:
+    - dict: Información sobre el usuario con más horas jugadas.
+    """
+    # Filtrar el DataFrame por el género específico
+    df_genre = df_ugenre[df_ugenre['genres'] == genero]
+
+    # Agrupar por usuario y año, sumar las horas jugadas
+    grouped_df = df_genre.groupby(['user_id', 'year_release'])['playtime_forever'].sum().reset_index()
+
+    # Encontrar al usuario con más horas jugadas
+    max_user = grouped_df.loc[grouped_df['playtime_forever'].idxmax()]['user_id']
+
+    # Filtrar el DataFrame original para obtener las horas jugadas por año del usuario máximo
+    max_user_df = df_ugenre[df_ugenre['user_id'] == max_user]
+    max_user_hours_by_year = max_user_df.groupby('year_release')['playtime_forever'].sum().reset_index()
+
+    # Crear la lista de la acumulación de horas jugadas por año
+    horas_jugadas_por_anio = [{"Año": int(anio), "Horas": int(horas)} for anio, horas in zip(max_user_hours_by_year['year_release'], max_user_hours_by_year['playtime_forever'])]
+
+    # Crear el diccionario de retorno
+    resultado = {"Usuario con más horas jugadas para Género " + genero: max_user, "Horas jugadas": horas_jugadas_por_anio}
+
+    return resultado
+
+
+
+def best_developer_year(year: int = Query(..., 
+                                          description="Año para el que se desea encontrar al mejor desarrollador", 
+                                          example=2022)):
+    """
+    Encuentra al desarrollador con más juegos recomendados para un año específico.
+
+    Parameters:
+    - year (int): Año para el que se desea encontrar al mejor desarrollador.
+
+    Returns:
+    - dict: Información sobre el desarrollador con más juegos recomendados.
+    """
+    # Verificar que las columnas necesarias existan en el DataFrame
+    required_columns = {'year_review', 'recommend', 'Sentiment_Score', 'app_name'}
+    if not required_columns.issubset(df_bdev.columns):
+        raise KeyError(f"Faltan las siguientes columnas necesarias: {required_columns - set(df_bdev.columns)}")
+
+    # Eliminar filas donde 'year_review' es nulo
+    df_bdev_cleaned = df_bdev.dropna(subset=['year_review'])
+
+    # Filtrar por año
+    df_filtered = df_bdev_cleaned[df_bdev_cleaned['year_review'] == year]
+
+    # Filtrar por recomendaciones positivas/neutrales
+    df_filtered = df_filtered[(df_filtered['recommend'] == True) & (df_filtered['Sentiment_Score'].isin([1, 2]))]
+
+    # Obtener el top 3 de juegos más recomendados
+    top_games = df_filtered.groupby('app_name')['recommend'].count().sort_values(ascending=False).head(3)
+
+    # Crear el diccionario de resultados en el formato deseado
+    top_games_dict = {f'Puesto {i+1}': juego for i, juego in enumerate(top_games.index)}
+
+    # Devolver el diccionario en el formato deseado
+    return top_games_dict
+
+
+def developer_reviews_analysis(developer: str = Query(..., 
+                                                      description="Nombre del desarrollador para el que se desea realizar el análisis")):
+    """
+    Realiza un análisis de sentimiento de las revisiones para un desarrollador específico.
+
+    Parameters:
+    - developer (str): Nombre del desarrollador.
+
+    Returns:
+    - dict: Resultados del análisis de sentimiento.
+    """
+    # Filtrar el dataframe por la desarrolladora especificada
+    developer_df = df_sentimiento[df_sentimiento['developer'] == developer]
+
+    # Sumar los valores de Sentiment_Score para cada categoría
+    negative_sum = developer_df[developer_df['Sentiment_Score'] == 0].shape[0]
+    neutral_sum = developer_df[developer_df['Sentiment_Score'] == 1].shape[0]
+    positive_sum = developer_df[developer_df['Sentiment_Score'] == 2].shape[0]
+
+    # Crear el diccionario de salida
+    result_dict = {
+        developer: {
+            'Negative': negative_sum,
+            'Neutral': neutral_sum,
+            'Positive': positive_sum
+        }
+    }
+
+    return result_dict
+
+
+
+def userdata(user_id: str = Query(..., 
+                                  description="ID del usuario para el que se desea obtener información")):
+    """
+    Obtiene información sobre el dinero gastado, el porcentaje de recomendación y la cantidad de items para un usuario específico.
+
+    Parameters:
+    - user_id (str): ID del usuario.
+
+    Returns:
+    - dict: Información sobre el usuario.
+    """
+    # Filtrar el DataFrame para obtener solo los datos del usuario especificado
+    user_data = df_bdev[df_bdev['user_id'] == user_id]
+
+    # Calcular el dinero gastado por el usuario
+    dinero_gastado = user_data['price'].sum()
+
+    # Calcular el porcentaje de recomendación
+    total_reviews = len(user_data)
+    recomendaciones_positivas = user_data['recommend'].sum()
+    porcentaje_recomendacion = (recomendaciones_positivas / total_reviews) * 100 if total_reviews > 0 else 0
+
+    # Calcular la cantidad de items
+    cantidad_items = user_data['item_id'].nunique()
+
+    # Crear el diccionario de resultados
+    resultado = {
+        "Usuario X": user_id,
+        "Dinero gastado": f"{dinero_gastado:.2f} USD",
+        "% de recomendación": f"{porcentaje_recomendacion:.2f}%",
+        "cantidad de items": cantidad_items
+    }
+
+    # Devolver el diccionario en el formato deseado
+    return resultado
+
+
+def developer(desarrollador: str = Query(..., 
+                                         description="Nombre del desarrollador para el que se desea obtener información")):
+    """
+    Obtiene la cantidad de items y el porcentaje de contenido Free por año según la empresa desarrolladora.
+
+    Parameters:
+    - desarrollador (str): Nombre del desarrollador.
+
+    Returns:
+    - dict: Información sobre la cantidad de items y el porcentaje de contenido Free por año.
+    """
+    # Filtrar el DataFrame para obtener solo los datos del desarrollador especificado
+    developer_data = df_bdev[df_bdev['developer'].str.lower() == desarrollador.lower()]
+
+    # Verificar si el DataFrame filtrado no está vacío
+    if developer_data.empty:
+        return f"No se encontraron datos para el desarrollador: {desarrollador}"
+
+    # Agrupar por año y calcular la cantidad de items y el porcentaje de contenido gratuito
+    grouped_data = developer_data.groupby('year_release').agg(
+        cantidad_items=('item_id', 'count'),
+        contenido_free_pct=('price', lambda x: (x == 0).mean() * 100)
+    ).reset_index()
+
+    # Renombrar las columnas para el formato deseado
+    grouped_data.columns = ['Año', 'Cantidad de Items', 'Contenido Free']
+
+    # Formatear el porcentaje de contenido gratuito
+    grouped_data['Contenido Free'] = grouped_data['Contenido Free'].map('{:.2f}%'.format)
+
+    # Devolver el DataFrame con el resultado
+    return grouped_data
+
+
+def recomendacion_usuario(user_id: str = Query(..., 
+                                              description="ID del usuario para el que se desean obtener recomendaciones")):
+    """
+    Obtiene una lista de 5 juegos recomendados para un usuario específico.
+
+    Parameters:
+    - user_id (str): ID del usuario.
+
+    Returns:
+    - list: Lista de 5 juegos recomendados.
+    """
+    # Verificar que el usuario existe en el DataFrame
+    if user_id not in df_bdev['user_id'].values:
+        return f"No se encontró el usuario con id: {user_id}"
+
+    # Obtener los datos de los juegos revisados por el usuario
+    juegos_usuario = df_bdev[df_bdev['user_id'] == user_id]
+
+    # Obtener los géneros de los juegos revisados por el usuario
+    generos_usuario = juegos_usuario['genres'].unique()
+
+    # Filtrar los juegos que coincidan con los géneros y que el usuario no haya revisado
+    juegos_recomendados = df_bdev[(df_bdev['genres'].isin(generos_usuario)) & 
+                                  (~df_bdev['item_id'].isin(juegos_usuario['item_id']))]
+
+    # Ordenar los juegos por el número de recomendaciones positivas
+    juegos_recomendados['positive_recommendations'] = juegos_recomendados.groupby('item_id')['recommend'].transform('sum')
+    top_recomendados = juegos_recomendados.sort_values(by='positive_recommendations', ascending=False).drop_duplicates('item_id').head(5)
+
+    # Crear la lista de juegos recomendados
+    lista_recomendaciones = top_recomendados['app_name'].tolist()
+
+    return lista_recomendaciones
--- main.py
+++ main.py
@@ -1,48 +1,138 @@
-from fastapi import FastAPI
-from typing import Optional, List
+import importlib
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse, HTMLResponse
+import funcion as f
+importlib.reload(f)
+
+
 
 app = FastAPI()
 
-# Supongamos que df_bdev es un DataFrame que contiene los datos necesarios
+@app.get(path="/", 
+         response_class=HTMLResponse,
+         tags=["Home"])
+def home():
+    return '''
+    <html>
+        <head>
+            <title>EJECUCION LOCAL API STEAM SISTEMA DE RECOMENDACION ITEMS</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                p {
+                    color: #666;
+                    text-align: center;
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Eres bienvenido a esta API de Steam</h1>
+            <p>INSTRUCCIONES:</p>
+            <p>Escribe <span style="background-color: lightgray;">/docs</span> al final de la URL de esta página para interactuar con la API.</p>
+            
+            <p style="color: #ff7e00; font-size: 20px; font-weight: bold; margin-top: 40px;">Estas son las consultas que podrás realizar al interactuar con la API</p>
+<ul>
+    <li><strong> Devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento.</strong></li>
+    <li><strong>Devuelve el top 3 de desarrolladores con juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos)</strong></li>
+    <li><strong>Obtener el top 3 de juegos más recomendados por año</strong></li>
+    <li><strong>Según el desarrollador, se devuelve un diccionario con el nombre del desarrollador como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor positivo o negativo.</strong></li>
+    <li><strong>Debe devolver cantidad de dinero gastado por el usuario, el porcentaje de recomendación en base a reviews.recommend y cantidad de items.</strong></li>
+    <li><strong>Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora</strong></li>
+    <li><strong>Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.</strong></li>
+</ul>
+        </body>
+    </html>
+    '''
 
-# Función userdata
-def userdata(user_id: str):
-    # Simulación de la función userdata utilizando df_bdev
-    return {"Usuario": user_id, "Dinero gastado": "200 USD", "% de recomendación": "20%", "cantidad de items": 5}
 
-# Función developer
-def developer(desarrollador: str):
-    # Simulación de la función developer utilizando df_bdev
-    return [{"Año": 2023, "Cantidad de Items": 50, "Contenido Free": "27%"}, 
-            {"Año": 2022, "Cantidad de Items": 45, "Contenido Free": "25%"}]
+@app.get('/user_for_genre',
+         description=""" <font color="blue">
+                    1. Haga click en aquí.<br>
+                    2. Ingrese el género .<br>
+                    3. Le va a mostrar los datos proporcionados.
+                    </font>
+                    """,
+         tags=["Consultas Generales"])
+def UserForGenre(genero: str = Query(..., 
+                            description="Género del videojuego", 
+                            example='Action')):
+    return f.UserForGenre(genero)
 
-# Función recomendacion_juego
-def recomendacion_juego(product_id: int):
-    # Simulación de la función recomendacion_juego utilizando df_bdev
-    return ["Juego 1", "Juego 2", "Juego 3", "Juego 4", "Juego 5"]
+@app.get('/best_developer',
+         description=""" <font color="blue">
+                    1. Haga click en "Quiero probar".<br>
+                    2. Ingrese el año en en la caja que se encuentra abajo.<br>
+                    3. Muevase hacia abajo ver las desarrolladoras menos recomendadas por los usuarios en ese año.
+                    </font>
+                    """,
+         tags=["Consultas Generales"])
+def best_developer_year(year: int = Query(...,
+                                description="Año para filtrar las desarrolladoras", 
+                                example=2010)):
+    return f.best_developer_year(year)
 
-# Función recomendacion_usuario
-def recomendacion_usuario(user_id: str):
-    # Simulación de la función recomendacion_usuario utilizando df_bdev
-    return ["Juego 1", "Juego 2", "Juego 3", "Juego 4", "Juego 5"]
+@app.get('/developer_reviews_analysis',
+         description=""" <font color="blue">
+                    INSTRUCCIONES<br>
+                    1. Haga click en "Quiero probar".<br>
+                    2. Ingrese el desarrollador en la caja que se encuentra abajo.<br>
+                    3. Verá la reseña de la desarroladora.
+                    </font>
+                    """,
+         tags=["Consultas Generales"])
+def developer_reviews_analysis(developer: str = Query(..., 
+                                description="Desarrollador del videojuego", 
+                                example='valve')):
+    return f.developer_reviews_analysis(developer)
 
-# Rutas de la API
-
-@app.get("/userdata/{user_id}")
-async def get_userdata(user_id: str):
-    return userdata(user_id)
-
-@app.get("/developer/{desarrollador}")
-async def get_developer(desarrollador: str):
-    return developer(desarrollador)
-
-@app.get("/recomendacion_juego/{product_id}")
-async def get_recomendacion_juego(product_id: int):
-    return recomendacion_juego(product_id)
-
-@app.get("/recomendacion_usuario/{user_id}")
-async def get_recomendacion_usuario(user_id: str):
-    return recomendacion_usuario(user_id)
+@app.get('/recomendacion_usuario',
+         description=""" <font color="blue">
+                    INSTRUCCIONES<br>
+                    1. Haga click en "Quiero probar".<br>
+                    2. Ingrese el juego en la caja que se encuentra abajo.<br>
+                    3. Muevase hacia abajo para obtener 5 recomendaciones de juegos similares.
+                    </font>
+                    """,       
+         tags=["Modelo de recomendación"])
+def recomendacion_usuario(user_id: str = Query(..., 
+                                    description="Nombre del juego para el cual se desea obtener recomendaciones", 
+                                    example='Killing Floor')):
+    resultado = f.recomendacion_usuario(user_id)
+    juegos_recomendados = resultado.tolist()
+    return JSONResponse(content={"juegos_recomendados": juegos_recomendados})
 
 
+@app.get('/user_data',
+         description=""" <font color="blue">
+                    1. Haga click en "Quiero probar".<br>
+                    2. Ingrese id de usuario en la caja que se encuentra abajo.<br>
+                    3. Muevase hacia abajo para ver los juegos más recomendados por los usuarios en ese año.
+                    </font>
+                    """,
+         tags=["Consultas Generales"])
+def userdata(user_id: str = Query(..., 
+                                  description="ID del usuario para el que se desea obtener información")):
+    resultado = f.userdata(user_id)
+    return resultado
 
+
+
+@app.get('/best_developer',
+         description=""" <font color="blue">
+                    1. Haga click en "Quiero probar".<br>
+                    2. Ingrese el año en en la caja que se encuentra abajo.<br>
+                    3. Muevase hacia abajo ver las desarrolladoras menos recomendadas por los usuarios en ese año.
+                    </font>
+                    """,
+         tags=["Consultas Generales"])
+def developer(desarrollador: str = Query(..., 
+                                         description="Nombre del desarrollador para el que se desea obtener información")):
+    return f.developer(desarrollador)
